c8d7ab1c9371d5d7e35a0c551dab0ce31fdc29a3
Philipp <github@philippeisen.de>
2016-01-14 16:43:04 +0100

    changed layout

--- UI.R
+++ UI.R
@@ -1,21 +1,26 @@
 shinyUI(fluidPage(
   titlePanel("NBA FantaViz"),
+
   
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "gender",
-                  label = "Choose Gender",
-                  choices = c("Male", "Female"),
-                  selected = "Male"),
-      selectInput(inputId = "type",
-                  label = "Choose Chart Type",
-                  choices = c("multiBarChart", "multiBarHorizontalChart"),
-                  selected = "multiBarChart"),
-      checkboxInput(inputId = "stack",
-                    label = strong("Stack Bars?"),
-                    value = FALSE)
+  fluidRow(
+    column(width = 4,
+           selectInput(inputId = "gender",
+                       label = "Choose Gender",
+                       choices = c("Male", "Female"),
+                       selected = "Male"),
+           selectInput(inputId = "type",
+                       label = "Choose Chart Type",
+                       choices = c("multiBarChart", "multiBarHorizontalChart"),
+                       selected = "multiBarChart"),
+           checkboxInput(inputId = "stack",
+                         label = strong("Stack Bars?"),
+                         value = FALSE)
     ),
-    mainPanel(
-      showOutput("myChart")
+    column(width = 5, offset = 0,
+           showOutput("myChart")
     )
-  )))
+  )
+))
+  
+  
+
