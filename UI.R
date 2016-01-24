e915828f1470b60de3d32f822070e008d3785edb
Philipp <github@philippeisen.de>
2016-01-24 23:34:21 +0100

    TabLayout

--- UI.R
+++ UI.R
@@ -6,30 +6,36 @@ require(rCharts)
 #Nothing yet.
 
 options(RCHART_LIB = 'polycharts')
-shinyUI(fluidPage(
-  fluidRow(
-    titlePanel("NBA FantaViz")
-  ),
-  fluidRow(
-  column(2,
-         
-#          selectInput("day", 
-#                      label = "Choose a game day",
-#                      choices = c("Points", "Points/Salary"),
-#                      selected = "Points"),
-         selectInput("var", 
-                     label = "Choose a variable",
-                     choices = c("Points", "Points/Salary"),
-                     selected = "Points")),
-  column(10,
-    rCharts::showOutput("chart1","nvd3")
-  )
-)
-#,
-#   fluidRow(
-#     column(7,
-#            DT::dataTableOutput('tab1')
-#     )
-#   )
-)
-)
+
+shinyUI(navbarPage("NBA FantaViz",
+   tabPanel("Players",
+            sidebarLayout(
+              sidebarPanel(
+                
+                       
+                       #          selectInput("day",
+                       #                      label = "Choose a game day",
+                       #                      choices = c("Points", "Points/Salary"),
+                       #                      selected = "Points"),
+                       selectInput("var",
+                                   label = "Choose a variable",
+                                   choices = c("Points", "Points/Salary"),
+                                   selected = "Points")),
+                mainPanel(
+                       rCharts::showOutput("chart1","nvd3")
+                )
+              )
+              #,
+              #   fluidRow(
+              #     column(7,
+              #            DT::dataTableOutput('tab1')
+              #     )
+              #   )
+            ),
+            
+   
+    tabPanel("Games",
+             fluidRow()
+             
+      )
+))
--- server/server_players.R
+++ server/server_players.R
@@ -1,39 +1,22 @@
-#*****Libraries******
-library(shiny)
-library(dplyr)
-#****Requires*******
-require(rCharts)
-#****Sources********
-#Nothing yet.
-
-
-options(RCHART_WIDTH = 700)
-#source("helpers.R")
-players <- read.csv("data/players.csv", sep = ";")
-players_barplot <- read.csv("data/players_barplot.csv", sep=";")
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  output$chart1 <- renderChart({
+output$chart1 <- renderChart({
     if(input$var=="Points"){
     p1 <- nPlot(Points ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart")
-    p1$addParams(height = 800, dom = 'chart1', 
+    p1$addParams(height = 800, dom = 'chart1',
                  title = "players")
     }
-    
+
     if(input$var=="Points/Salary"){
     p1 <- nPlot(Points.Salary ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart", stacked=TRUE)
-    p1$addParams(height = 800, dom = 'chart1', 
+    p1$addParams(height = 800, dom = 'chart1',
                    title = "players")
     }
-    
+
     p1$chart(stacked = TRUE,margin = list(left=200))
     p1$xAxis(width = 300)
     return(p1)
-    
+
   })
   output$tab1 <- DT::renderDataTable({
     players.tableData <- data.frame(players$Name,players$Position,players$Salary)
     DT::datatable(players.tableData,options =list(paging = FALSE))
-  })
   })
