473d46156ce3235135eb231a5bcf2f9ad5197dcb
Philipp <github@philippeisen.de>
2016-01-25 00:00:32 +0100

    changed colors of chart

--- server.R
+++ server.R
@@ -16,18 +16,15 @@ players_barplot <- read.csv("data/players_barplot.csv", sep=";")
 shinyServer(function(input, output) {
   output$chart1 <- renderChart({
     if(input$var=="Points"){
-    p1 <- nPlot(Points ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart")
-    p1$addParams(height = 800, dom = 'chart1', 
-                 title = "players")
+     p1 <- nPlot(Points ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart")
     }
     
     if(input$var=="Points/Salary"){
-    p1 <- nPlot(Points.Salary ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart", stacked=TRUE)
-    p1$addParams(height = 800, dom = 'chart1', 
-                   title = "players")
-    }
+     p1 <- nPlot(Points.Salary ~ Name, group = 'Type', data = players_barplot, type = "multiBarHorizontalChart", stacked=TRUE)
     
-    p1$chart(stacked = TRUE,margin = list(left=200))
+    }
+    p1$addParams(height = 800, dom = 'chart1', title = "players")
+    p1$chart(stacked = TRUE,margin = list(left=150, right = 70), color = c('#ffb729','#ff353e','#519399'))
     p1$xAxis(width = 300)
     return(p1)
     
