a238f3e45975768b4b2218e52219a5113d4fc03c
Hugo <santanahugo752@gmail.com>
2016-01-25 18:47:24 +0100

    Updates on gameday server

--- server.R
+++ server.R
@@ -91,13 +91,28 @@ shinyServer(function(input, output) {
   })
   
   # Generate a summary of the dataset
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
+  output$summary <- DT::renderDataTable(
+    DT::datatable(gameday, options = list(paging = FALSE, searching=FALSE, autoWidth = TRUE,
+                                          columnDefs = list(list(width = '60px', targets = "_all"))))
+  )
+    
+  #output$summary <- renderDataTable({
+  #  dataset <- datasetInput()
+  #  })
   # Show the first "n" observations set to 20 default
-  output$view <- renderTable({
-    head(datasetInput(), n = 20)
+  #output$view <- renderTable({
+  #  head(datasetInput(), n = 20)
+ 
+  
+    output$games1 <- renderChart({ 
+  
+      p2 <- nPlot(Points ~ Team, data = totalPoints_Game, type = "multiBarChart")
+      p2$yAxis(axisLabel = "Points")
+      p2$xAxis(axisLabel = "Teams")
+      p2$addParams(height = 300, dom = 'games1', title = "games" )
+      p2$chart(showControls=FALSE, margin = list(left=100, right = 70, bottom = 100))
+      #options(RCHART_WIDTH = 400)
+      return(p2)
+      
   })
 })
